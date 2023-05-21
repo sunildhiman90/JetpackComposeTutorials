b0b23933e5519bf9eae55c6e2984e5e0bfffe3ab
Sunil Kumar <sunil.dhiman90@gmail.com>
2023-05-22 00:23:04 +0530

    Initial commit

---
+++ app/src/main/java/com/codingambitions/composetutorial2/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.codingambitions.composetutorial2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.codingambitions.composetutorial2.ui.theme.ComposeTutorial2Theme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTutorial2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        val username = remember {
+                            mutableStateOf(TextFieldValue())
+                        }
+                        val password = remember {
+                            mutableStateOf(TextFieldValue())
+                        }
+                        Text(text = "Login", style = MaterialTheme.typography.displaySmall)
+                        Spacer(modifier = Modifier.height(36.dp))
+
+                        OutlinedTextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = username.value, onValueChange = {
+                                username.value = it
+                            },
+                            label = { Text(text = "Username") },
+                            visualTransformation = VisualTransformation.None
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        OutlinedTextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = password.value, onValueChange = {
+                                password.value = it
+                            },
+                            label = { Text(text = "Password") },
+                            visualTransformation = PasswordVisualTransformation(),
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Button(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(50.dp), onClick = { /*TODO*/ }) {
+                            Text(text = "Login")
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeTutorial2Theme {
+        Greeting("Android")
+    }
+}
